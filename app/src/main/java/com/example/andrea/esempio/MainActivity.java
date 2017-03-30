3cc86e33fb62f2c8eafef2d9a00134473aa5b95f
theghiro <emanuele.leggeri@gmail.com>
2017-03-30 12:21:51 +0200

    andrea

--- app/src/main/java/com/example/andrea/esempio/MainActivity.java
+++ app/src/main/java/com/example/andrea/esempio/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.andrea.esempio;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
 }
